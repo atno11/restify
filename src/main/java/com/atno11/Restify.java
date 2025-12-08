a1842ca4771af20621a858f94bcc59f980171eb2
atno11 <atno@albusnexus.com>
2025-12-08 11:55:31 -0300

    :zap: config initialization

--- src/main/java/com/atno11/Restify.java
+++ src/main/java/com/atno11/Restify.java
@@ -1,24 +1,39 @@
 package com.atno11;
 
+import com.atno11.configs.ApiConfig;
+import com.atno11.handlers.ConfigHandler;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import net.fabricmc.api.EnvType;
 import net.fabricmc.api.ModInitializer;
 
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.server.MinecraftServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class Restify implements ModInitializer {
 	public static final String MOD_ID = "restify";
-
-	// This logger is used to write text to the console and the log file.
-	// It is considered best practice to use your mod id as the logger's name.
-	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
-
+    public static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
+    public static MinecraftServer SERVER;
+    public static ConfigHandler<ApiConfig> apiConfig = new ConfigHandler<>(ApiConfig.class);
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
+    if (FabricLoader.getInstance().getEnvironmentType() == EnvType.CLIENT) {
+        LOGGER.warn("Client environment detected — skipping mod initialization.");
+        return;
+    }
 
-		LOGGER.info("Hello Fabric world!");
+    LOGGER.info("RESTIFY is Starting!");
+    // Load api-settings config file;
+    apiConfig.load();
+
+    ServerLifecycleEvents.SERVER_STARTED.register(server -> {
+        SERVER = server;
+        LOGGER.info("Fabric server instance initialized successfully");
+        ApiConfig api = apiConfig.getData();
+    });
 	}
 }
