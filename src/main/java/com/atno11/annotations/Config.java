41544a4068e18c348b308cfa16571f541e4ecae2
atno11 <atno@albusnexus.com>
2025-12-08 11:53:42 -0300

    :sparkles: config annotation

---
+++ src/main/java/com/atno11/annotations/Config.java
@@ -0,0 +1,12 @@
+package com.atno11.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Config {
+    String fileName();
+}
